0813da63eb1d2e20ab66e3a0771e5630af5cac2a
BreadMaker y la CTM <breadmaker666@gmail.com>
2018-02-06 01:44:02 -0300

    Optimizes code, it should work fine on mobile now.

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,49 @@
 /*jshint -W056 */
 var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-var tempo = 0;
 
-function beep(tune) {
-    if (tune.length === 0) {
-        $("#tune, #play").removeClass("disabled").removeAttr("disabled");
-        return;
+function composeTune(tuneArr) {
+    var tune = [];
+    while (tuneArr.length > 0) {
+        tune.push({
+            frequency: tuneArr[0],
+            duration: tuneArr[1]
+        });
+        tuneArr = tuneArr.splice(2);
     }
-    var oscillator = audioCtx.createOscillator();
-    var gainNode = audioCtx.createGain();
+    return tune;
+}
 
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-
-    gainNode.gain.value = 0.25;
-    oscillator.frequency.value = tune[0];
-    oscillator.type = "square";
-
-    oscillator.start();
-
-    var duration = (tune[1] * 60000) / tempo;
-
-    setTimeout(function () {
-        oscillator.stop();
-        beep(tune.splice(2));
-    }, duration);
+function play(tempo, tune) {
+    var oscillator, baseTime = audioCtx.currentTime,
+        arrayLength = tune.length,
+        playlength = 0,
+        gainNode = audioCtx.createGain();
+    for (var i = 0; i < arrayLength; i++) {
+        oscillator = audioCtx.createOscillator();
+        oscillator.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+        gainNode.gain.value = 0.125;
+        playlength = 1 / (tempo / 60) * tune[i].duration;
+        oscillator.type = "square";
+        oscillator.frequency.value = tune[i].frequency;
+        if (i == arrayLength - 1) {
+            /*jshint -W083 */
+            oscillator.onended = function (ev) {
+                $("#tune, #play").removeClass("disabled").removeAttr("disabled");
+            };
+        }
+        oscillator.start(baseTime);
+        oscillator.stop(baseTime + playlength);
+        baseTime += playlength;
+    }
 }
 
 $(document).ready(function () {
     $("#tune-form").submit(function (e) {
         e.preventDefault();
         var inputArr = $("#tune").val().split(" ");
-        tempo = inputArr[0];
         $("#tune, #play").addClass("disabled").attr("disabled",
             "disabled");
-        beep(inputArr.splice(1));
+        play(inputArr[0], composeTune(inputArr.splice(1)));
     });
 });
