2a2f0d36af1d5bdbec4d4740d6b85a8caecc4ee4
BreadMaker <breadmaker@ctm-laptop>
2021-05-16 00:36:32 -0400

    v0.5.0: Complete overhaul, now using plain Javascript and Bootstrap.

--- src/js/main.js
+++ src/js/main.js
@@ -1,85 +1,221 @@
-/*jshint -W056 */
 var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-var requestId;
+var requestId, bootstrap, ClipboardJS;
+
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
 
 function createTuneObj(tuneStr) {
-    var tuneArr = tuneStr.split(" ");
-    var tuneObj = {};
-    tuneObj.tempo = parseInt(tuneArr[0]);
-    tuneObj.tune = [];
-    tuneArr = tuneArr.splice(1);
-    while (tuneArr.length > 0) {
-        tuneObj.tune.push({
-            frequency: parseInt(tuneArr[0]),
-            duration: parseInt(tuneArr[1])
-        });
-        tuneArr = tuneArr.splice(2);
-    }
-    return tuneObj;
+  var tuneArr = tuneStr.split(" ");
+  var tuneObj = Object();
+  tuneObj.tempo = parseInt(tuneArr[0]);
+  tuneObj.tune = [];
+  tuneArr = tuneArr.splice(1);
+  while (tuneArr.length > 0) {
+    tuneObj.tune.push({
+      frequency: parseInt(tuneArr[0]),
+      duration: parseInt(tuneArr[1])
+    });
+    tuneArr = tuneArr.splice(2);
+  }
+  return tuneObj;
 }
 
 function calculateDuration(tuneObj) {
-    var playlength = 0;
-    for (var i = 0; i < tuneObj.tune.length; i++) {
-        playlength += 1 / (tuneObj.tempo / 60) * tuneObj.tune[i].duration;
-    }
-    $("#notes").text(tuneObj.tune.length);
-    $("#duration").text(Math.round(playlength * 1000));
+  var playlength = Number();
+  for (var i = 0; i < tuneObj.tune.length; i++) {
+    playlength += 1 / (tuneObj.tempo / 60) * tuneObj.tune[i].duration;
+  }
+  document.getElementById("notes").textContent = tuneObj.tune.length;
+  document.getElementById("duration").textContent = Math.round(playlength * 1000) + "ms";
+  document.getElementById("duration").title = (playlength % 1 == 0 ? playlength : playlength.toFixed(3)) + "s";
 }
 
 function play(tuneObj) {
-    if (isNaN(tuneObj.tempo)) {
-        $("#tune, #play").removeClass("disabled").removeAttr("disabled");
-        return;
+  let baseTime = audioCtx.currentTime,
+    arrayLength = tuneObj.tune.length,
+    playlength = 0,
+    gainNode = audioCtx.createGain();
+  for (var i = 0; i < arrayLength; i++) {
+    let oscillator = audioCtx.createOscillator();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    gainNode.gain.value = 0.125;
+    playlength = 1 / (tuneObj.tempo / 60) * tuneObj.tune[i].duration;
+    oscillator.type = "square";
+    oscillator.frequency.value = tuneObj.tune[i].frequency;
+    if (i == arrayLength - 1) {
+      oscillator.onended = function() {
+        document.getElementById("tune").classList.remove("disabled");
+        document.getElementById("tune").removeAttribute("disabled");
+        document.getElementById("play").classList.remove("d-none");
+        document.getElementById("stop").classList.add("d-none");
+        cancelAnimationFrame(requestId);
+      };
     }
-    var baseTime = audioCtx.currentTime,
-        arrayLength = tuneObj.tune.length,
-        playlength = 0,
-        gainNode = audioCtx.createGain();
-    for (var i = 0; i < arrayLength; i++) {
-        var oscillator = audioCtx.createOscillator();
-        oscillator.connect(gainNode);
-        gainNode.connect(audioCtx.destination);
-        gainNode.gain.value = 0.125;
-        playlength = 1 / (tuneObj.tempo / 60) * tuneObj.tune[i].duration;
-        oscillator.type = "square";
-        oscillator.frequency.value = tuneObj.tune[i].frequency;
-        if (i == arrayLength - 1) {
-            /*jshint -W083 */
-            oscillator.onended = function (ev) {
-                $("#tune, #play").removeClass("disabled").removeAttr("disabled");
-                cancelAnimationFrame(requestId);
-            };
-        }
-        oscillator.start(baseTime);
-        oscillator.stop(baseTime + playlength);
-        baseTime += playlength;
+    tuneObj.tune[i].oscillator = oscillator;
+    oscillator.start(baseTime);
+    oscillator.stop(baseTime + playlength);
+    baseTime += playlength;
+  }
+  document.getElementById("stop").addEventListener("click", () => {
+    for (let note of tuneObj.tune) {
+      note.oscillator.stop();
     }
+  }, {
+    once: true
+  });
 }
 
-$(document).ready(function () {
-    var clipboard = new ClipboardJS("#copy", {
-        text: function (trigger) {
-            return $("#tune").val();
-        }
+function loadSelect() {
+  let data = [{
+      title: "Default tune",
+      tune: "480 440 1"
+    }, {
+      title: "Mario Coin FX",
+      tune: "600 988 1 1319 4"
+    }, {
+      title: "Mario Bros Powerup FX",
+      tune: "1750 523 1 392 1 523 1 659 1 784 1 1047 1 784 1 415 1 523 1 622 1 831 1 622 1 831 1 1046 1 1244 1 1661 1 1244 1 466 1 587 1 698 1 932 1 1175 1 1397 1 1865 1 1397 1"
+    }, {
+      title: "Mario Bros Music Intro",
+      tune: "410 668 1 668 1 0 1 668 1 0 1 522 1 668 1 0 1 784 2 0 2 392 2"
+    }, {
+      title: "Imperial March 1st Verse",
+      tune: "550 392 3 0 1 392 3 0 1 392 3 0 1 311 3 466 1 392 3 0 1 311 3 466 1 392 6 0 2 587 3 0 1 587 3 0 1 587 3 0 1 622 3 466 1 370 3 0 1 311 3 466 1 392 4"
+    }, {
+      title: "Close Encounters",
+      tune: "400 880 2 988 2 783 2 392 2 587 3"
+    }, {
+      title: "Wolfestein 3D Music Intro",
+      tune: "300 131 1 196 1 196 1 196 1 294 1 196 1 294 1 196 1 131 2"
+    }],
+    tuneSelect = document.getElementById("tune");
+  tuneSelect.remove(0);
+  for (let item of data) {
+    let option = document.createElement("option");
+    option.textContent = item.title;
+    option.value = item.tune;
+    tuneSelect.appendChild(option);
+  }
+  let option = document.createElement("option");
+  option.textContent = "Custom input";
+  option.value = "";
+  tuneSelect.appendChild(option);
+  tuneSelect.removeAttribute("disabled");
+  document.getElementById("tune-input").value = tuneSelect.value;
+}
+
+function showWarningMessage(message) {
+  document.getElementById("warning-alert").classList.remove("d-none");
+  document.getElementById("tune-info").classList.add("d-none");
+  document.getElementById("warning-alert-message").textContent = message;
+  document.getElementById("play").classList.add("disabled");
+  document.getElementById("play").setAttribute("disabled", "disabled");
+}
+
+function validateTuneInput(input) {
+  let inputArray = input.split(" "),
+    isAllNumbers = true,
+    containsBlanks = false,
+    warningAlertContainer = document.getElementById("warning-alert"),
+    tuneInfoContainer = document.getElementById("tune-info");
+  for (let item of inputArray) {
+    if (isNaN(item)) {
+      isAllNumbers = false;
+      break;
+    } else if (item === "") {
+      containsBlanks = true;
+      break;
+    }
+  }
+  if (input.length == 0) {
+    showWarningMessage("Input at least one note.");
+  } else if (!isAllNumbers) {
+    showWarningMessage("Please enter only numbers.");
+  } else if (containsBlanks) {
+    showWarningMessage("Only one space between numbers.");
+  } else {
+    if (inputArray.length < 3) {
+      showWarningMessage("Input at least one note.");
+    } else if (inputArray.length >= 3 && inputArray.length % 2 == 0) {
+      showWarningMessage("One more number to define a note.");
+    } else {
+      document.getElementById("play").classList.remove("disabled");
+      document.getElementById("play").removeAttribute("disabled");
+      warningAlertContainer.classList.add("d-none");
+      tuneInfoContainer.classList.remove("d-none");
+      calculateDuration(createTuneObj(input));
+    }
+  }
+}
+
+function addFormEvents() {
+  let tuneSelect = document.getElementById("tune"),
+    tuneInput = document.getElementById("tune-input"),
+    warningAlertContainer = document.getElementById("warning-alert"),
+    tuneInfoContainer = document.getElementById("tune-info");
+  tuneSelect.addEventListener("change", () => {
+    tuneInput.value = tuneSelect.value;
+    tuneInput.parentNode.dataset.replicatedValue = tuneInput.value;
+    if (tuneSelect.value === "") {
+      document.getElementById("play").classList.add("disabled");
+      document.getElementById("play").setAttribute("disabled", "disabled");
+      warningAlertContainer.classList.remove("d-none");
+      tuneInfoContainer.classList.add("d-none");
+      tuneInput.removeAttribute("readonly");
+    } else {
+      document.getElementById("play").classList.remove("disabled");
+      document.getElementById("play").removeAttribute("disabled");
+      warningAlertContainer.classList.add("d-none");
+      tuneInfoContainer.classList.remove("d-none");
+      calculateDuration(createTuneObj(tuneSelect.value));
+      tuneInput.setAttribute("readonly", "readonly");
+    }
+  });
+  tuneInput.addEventListener("input", () => {
+    tuneInput.parentNode.dataset.replicatedValue = tuneInput.value;
+    validateTuneInput(tuneInput.value);
+  });
+  tuneInput.addEventListener("paste", () => {
+    tuneInput.parentNode.dataset.replicatedValue = tuneInput.value;
+    validateTuneInput(tuneInput.value);
+  });
+  document.getElementById("tune-form").addEventListener("submit", event => {
+    event.preventDefault();
+    document.getElementById("tune").classList.add("disabled");
+    document.getElementById("tune").setAttribute("disabled", "disabled");
+    document.getElementById("play").classList.add("d-none");
+    document.getElementById("stop").classList.remove("d-none");
+    requestId = requestAnimationFrame(() => {
+      play(createTuneObj(tuneInput.value));
     });
-    clipboard.on('success', function () {
-        $("#copy").popup("change content", "Copied!");
+  });
+}
+
+ready(function() {
+  document.getElementById("tune-input").value = "Loading...";
+  loadSelect();
+  addFormEvents();
+  calculateDuration(createTuneObj(document.getElementById("tune").value));
+  new ClipboardJS("#copy", {
+    text: function() {
+      return document.getElementById("tune-input").value;
+    }
+  }).on("success", function() {
+    var tooltip = new bootstrap.Tooltip(document.getElementById("copy"), {
+      title: "Copied!",
+      trigger: "hover"
     });
-    $("#copy").popup();
-    $("#tune").dropdown({
-        onChange: function (text, value) {
-            calculateDuration(createTuneObj(value));
-        },
-        allowAdditions: true,
-        hideAdditions: false
-    }).trigger("change");
-    $("#tune-form").submit(function (e) {
-        e.preventDefault();
-        $("#tune, #play").addClass("disabled").attr("disabled",
-            "disabled");
-        requestId = requestAnimationFrame(play(createTuneObj($("#tune")
-            .val())));
+    tooltip.show();
+    document.getElementById("copy").addEventListener("hidden.bs.tooltip", () => {
+      tooltip.dispose();
+    }, {
+      once: true
     });
-    calculateDuration(createTuneObj($("#tune").val()));
+  });
 });
